eba177945dc121d066c981ea784991bf79c93c0e
Robert Weekes <weekes.robert@gmail.com>
2024-06-05 17:58:15 -0700

    done

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,21 @@
 const Person = require('./person');
 
-// Your code here 
+class Teacher extends Person {
+    constructor(firstName, lastName, subject, yearsOfExperience) {
+        super(firstName, lastName);
+        this.subject = subject;
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    static combinedYearsOfExperience(teachers) {
+        let years = 0;
+        teachers.forEach(teacher => {
+            years += teacher.yearsOfExperience;
+        });
+
+        return years;
+    }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
